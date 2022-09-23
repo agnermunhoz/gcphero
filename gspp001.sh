e53f1711b56f09f2b5a36a5fecf08f034912f240
Agner Munhoz <agner.munhoz@gmail.com>
2022-09-23 12:03:53 -0300

    Update gspp001.sh

--- gspp001.sh
+++ gspp001.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-gcloud auth list
+PROJECTID=$(gcloud config get project)
 read -p 'Enter compute zone: ' ZONE
 REGION=${ZONE::-2}
 gcloud config set compute/region $REGION
 gcloud config set compute/zone $ZONE
-gcloud compute instances create gcelab \
-  --zone=$ZONE \
-  --machine-type=e2-medium \
-  --tags=http-server \
-  --metadata=startup-script='#!/bin/bash
-     apt-get update
-     apt-get install nginx -y' &
 gcloud compute instances create gcelab2 \
    --machine-type=e2-medium \
    --zone=$ZONE &
