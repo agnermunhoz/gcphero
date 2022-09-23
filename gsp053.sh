288720bedc2f637d8661e90a56e79ccd2c750788
Agner Munhoz <agner.munhoz@gmail.com>
2022-09-23 02:23:46 -0300

    Update gsp053.sh

--- gsp053.sh
+++ gsp053.sh
@@ -17,8 +17,8 @@ kubectl create secret generic tls-certs --from-file tls/
 kubectl create configmap nginx-frontend-conf --from-file=nginx/frontend.conf
 kubectl create -f deployments/frontend.yaml
 kubectl create -f services/frontend.yaml
-read -p 'Check "Create a Kubernetes cluster and deployments (Auth, Hello, and Frontend)"' CONTINUE
+echo 'Check "Create a Kubernetes cluster and deployments (Auth, Hello, and Frontend)"'
 kubectl scale deployment hello --replicas=3
 kubectl create -f deployments/hello-canary.yaml
-read -p 'Check "Canary Deployment"' CONTINUE
+echo 'Check "Canary Deployment"'
 echo "Lab ended!"
