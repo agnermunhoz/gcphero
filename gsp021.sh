b324577a1f028050a0989fe7c8486267488083f9
Agner Munhoz <agner.munhoz@gmail.com>
2022-09-23 02:11:03 -0300

    Update gsp021.sh

--- gsp021.sh
+++ gsp021.sh
@@ -6,19 +6,19 @@ gcloud container clusters create io
 cd orchestrate-with-kubernetes/kubernetes
 kubectl create deployment nginx --image=nginx:1.10.0
 kubectl expose deployment nginx --port 80 --type LoadBalancer
-read -p 'Check "Create a Kubernetes cluster and launch Nginx container"' CONTINUE
+echo 'Check "Create a Kubernetes cluster and launch Nginx container"'
 kubectl create -f pods/monolith.yaml
 cd ~/orchestrate-with-kubernetes/kubernetes
 kubectl create secret generic tls-certs --from-file tls/
 kubectl create configmap nginx-proxy-conf --from-file nginx/proxy.conf
 kubectl create -f pods/secure-monolith.yaml
 kubectl create -f services/monolith.yaml
-read -p 'Check "Create Monolith pods and service"' CONTINUE
+echo 'Check "Create Monolith pods and service"'
 gcloud compute firewall-rules create allow-monolith-nodeport \
   --allow=tcp:31000
-read -p 'Check "Allow traffic to the monolith service on the exposed nodeport"' CONTINUE
+echo 'Check "Allow traffic to the monolith service on the exposed nodeport"'
 kubectl label pods secure-monolith 'secure=enabled'
-read -p 'Check "Adding Labels to Pods"' CONTINUE
+echo 'Check "Adding Labels to Pods"'
 kubectl create -f deployments/auth.yaml
 kubectl create -f services/auth.yaml
 kubectl create -f deployments/hello.yaml
@@ -26,6 +26,6 @@ kubectl create -f services/hello.yaml
 kubectl create configmap nginx-frontend-conf --from-file=nginx/frontend.conf
 kubectl create -f deployments/frontend.yaml
 kubectl create -f services/frontend.yaml
-read -p 'Check "Creating Deployments (Auth, Hello and Frontend)"' CONTINUE
+echo 'Check "Creating Deployments (Auth, Hello and Frontend)"'
 
 echo "Lab ended!"
