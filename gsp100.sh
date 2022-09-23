edd14bfd4d611ea1aa5c122bd9df238bfbe3ba0a
Agner Munhoz <agner.munhoz@gmail.com>
2022-09-23 00:20:40 -0300

    Update gsp100.sh

--- gsp100.sh
+++ gsp100.sh
@@ -7,12 +7,16 @@ gcloud container clusters create --machine-type=e2-medium --zone=$REGION-$ZONE l
 read -p 'Check "Create a GKE cluster"' CONTINUE
 gcloud container clusters get-credentials lab-cluster 
 kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:1.0
-until echo "$(kubectl get deployments hello-server)" | grep "1/1"; do
+until echo "$(kubectl get pod)" | grep -q "1/1"; do
    sleep 1
    echo "Waiting for Deployment to be ready......................."
 done
 read -p 'Check "Create a new Deployment: hello-server"' CONTINUE
 kubectl expose deployment hello-server --type=LoadBalancer --port 8080
+while echo "$(kubectl get services)" | grep -q "pending"; do
+   sleep 1
+   echo "Waiting for Service to be ready......................."
+done
 read -p 'Check "Create a Kubernetes Service"' CONTINUE
 echo "Type Y to confirme cluster deletion"
 gcloud container clusters delete lab-cluster 
